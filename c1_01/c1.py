066040a82b849a80964f66fd0316fdff7aae8b83
executiveforce <870055678@qq.com>
2017-09-07 02:10:09 +0800

    9/7

---
+++ c1_01/c1.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+def pachou():
+    content = requests.get('http://www.qiushibaike.com').content
+    soup = BeautifulSoup(content,'html.parser')
+
+    for div in soup.find_all('div',{'class':'content'}):
+        print div.text.strip()
+
+if __name__=='__main__':
+    print 'Hello Python'
