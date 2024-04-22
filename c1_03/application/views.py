45dbcdbfe015968c5d4f0af10b6f9f4f63d0d42b
Allen <870055678@qq.com>
2024-04-22 15:46:27 +0800

    Delete c1_03 directory

--- c1_03/application/views.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding=UTF-8 -*-
-
-from application import app
-
-@app.route('/')
-def index():
-    return 'Hello'
--- c1_03/runserver.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding=UTF-8 -*-
-
-from application import app
-
-if __name__=='__main__':
-    app.run()
